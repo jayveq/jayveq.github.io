e8de246645f1258827a96b162c102d9403b280a6
Joshua Schmitz <90041829+jayveq@users.noreply.github.com>
2023-10-25 16:14:24 +0200

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,5 +1,6 @@
 const matrixContainer = document.getElementById("matrix-container");
 const matrix = document.getElementById("matrix");
+const characters = []; // Define characters as a global array
 
 let running = false;
 let stopped = false;
@@ -27,12 +28,10 @@ function startMatrix() {
     running = true;
     matrixContainer.style.display = "block";
 
-    const characters = [];
-
     // Generate and add characters to the matrix
     for (let i = 0; i < 100; i++) {
         const character = createCharacter();
-        characters.push(character);
+        characters.push(character); // Add the character to the global array
         matrix.appendChild(character);
     }
 
